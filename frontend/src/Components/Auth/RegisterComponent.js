20a465d6186c53c0c58c0bf646321f1fc952b0b5
Oskar Biwejnis <01169534@pw.edu.pl>
2024-05-21 12:31:08 +0200

    change logo in RegisterComponent

--- frontend/src/Components/Auth/RegisterComponent.js
+++ frontend/src/Components/Auth/RegisterComponent.js
@@ -37,12 +37,7 @@ const RegisterComponent = () => {
           href="#"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          Flowbite
+          <img src={logo} width={150} className="mx-auto" alt="Logo" />
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
